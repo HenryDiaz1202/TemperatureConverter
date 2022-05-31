2d7cccfc10ce6b1afad2beab6f1aa39777de6655
HenryDiaz1202 <henrydiaz1202@gmail.com>
2022-05-30 22:53:46 -0600

    Diseño de la vista de biografías hecha, finalizada

--- app/src/main/java/com/henry/formulessolve/BiografiasActivity.java
+++ app/src/main/java/com/henry/formulessolve/BiografiasActivity.java
@@ -3,12 +3,29 @@ package com.henry.formulessolve;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class BiografiasActivity extends AppCompatActivity {
+
+    private static final String TAG = "BiografiasActivity";
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biografias);
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
     }
+
+    public void back(View view) {
+        finish();
+    }
+
 }
